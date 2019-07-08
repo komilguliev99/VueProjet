24e5aba40549dcd0d490e094d3e434bed9ee22ba
Komil Guliev <komil-guliev@mail.ru>
2019-07-08 16:51:55 +0300

    Creating pages: detail page of ad

--- src/components/Ads/Ad.vue
+++ src/components/Ads/Ad.vue
@@ -2,7 +2,25 @@
     <v-container>
         <v-layout>
             <v-flex xs12>
-                <h1>Ad</h1>
+                <v-card>
+                    <v-img
+                        src="https://www.nastol.com.ua/pic/201209/1920x1200/nastol.com.ua-32366.jpg"
+                        height="300px"
+                    ></v-img>
+                    <v-card-text>
+                        <h1 class="text--primary">Cute Squirrel</h1>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                            Voluptate incidunt consequuntur nihil architecto possimus 
+                            quis natus minima eligendi veniam. Sed, est vel. 
+                            Rem obcaecati ducimus, officiis ut similique voluptatem. 
+                            Facere.</p>
+                    </v-card-text>
+                    <v-card-actions>
+                        <v-spacer></v-spacer>
+                        <v-btn class="warning" flat>Edit</v-btn>
+                        <v-btn class="success">Buy</v-btn>
+                    </v-card-actions>
+                </v-card>
             </v-flex>
         </v-layout>
     </v-container>
