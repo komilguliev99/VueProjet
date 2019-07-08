d6e400508175c4519a5123b17b7b5d256129a11c
Komil Guliev <komil-guliev@mail.ru>
2019-07-08 10:27:56 +0300

    Creating App & UI Login

---
+++ src/components/Ads/Ad.vue
@@ -0,0 +1,20 @@
+<template>
+    <v-container>
+        <v-layout>
+            <v-flex xs12>
+                <h1>Ad</h1>
+            </v-flex>
+        </v-layout>
+    </v-container>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            
+        }
+    }
+}
+</script>
+
