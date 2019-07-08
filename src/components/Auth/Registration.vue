b37e40294c57c00c36492a6a3e5f85050106a58a
Komil Guliev <komil-guliev@mail.ru>
2019-07-08 10:38:17 +0300

    Registration form

--- src/components/Auth/Registration.vue
+++ src/components/Auth/Registration.vue
@@ -1,19 +1,93 @@
 <template>
-    <v-container>
-        <v-layout>
-            <v-flex xs12>
-                <h1>Registration</h1>
+    <v-container fluid fill-height>
+        <v-layout align-center justify-center>
+            <v-flex xs12 sm8 md6>
+                <v-card class="elevation-12">
+                <v-toolbar dark color="primary">
+                    <v-toolbar-title>Registration form</v-toolbar-title>
+                </v-toolbar>
+                <v-card-text>
+                    <v-form v-model="valid" ref="form" validation>
+                        <v-text-field 
+                            prepend-icon="person"
+                            name="email"
+                            label="E-mail"
+                            :rules="emailRules"
+                            type="text"
+                            v-model="email"
+                        ></v-text-field>
+                        <v-text-field
+                            id="password"
+                            prepend-icon="lock"
+                            name="password"
+                            label="Password"
+                            type="password"
+                            :counter="6"
+                            :rules="passwordRules"
+                            v-model="password"
+                        ></v-text-field>
+                        <v-text-field
+                            id="confirm-password"
+                            prepend-icon="lock"
+                            name="confirm-password"
+                            label="Confirm password"
+                            type="password"
+                            :counter="6"
+                            :rules="confirmPasswordRules"
+                            v-model="confirmPassword"
+                        ></v-text-field>
+                    </v-form>
+                </v-card-text>
+                <v-card-actions>
+                    <v-spacer></v-spacer>
+                    <v-btn
+                        color="primary"
+                        @click="onSubmit"
+                        :disabled="!valid"
+                    >Sign up</v-btn>
+                </v-card-actions>
+                </v-card>
             </v-flex>
         </v-layout>
     </v-container>
 </template>
 
 <script>
+
+const emailRegex = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/
+
 export default {
     data () {
         return {
-            
+            email: '',
+            password: '',
+            confirmPassword: '',
+            valid: false,
+            emailRules: [
+                v => !!v || 'E-mail is required',
+                v => emailRegex.test(v) || 'E-mail must be valid'
+            ],
+            passwordRules: [
+                v => !!v || 'Password is required',
+                v => (v && v.length >= 6) || 'Password must be equal or more than 6 characters'
+            ],
+            confirmPasswordRules: [
+                v => !!v || 'Password is required',
+                v => (v === this.password) || 'Password does not match'
+            ]
         }
+    },
+    methods: {
+        onSubmit () {
+            if (this.$refs.form.validate()) {
+                const user = {
+                    email: this.email,
+                    password: this.password
+                }
+
+                console.log(user);
+            }
+        } 
     }
 }
 </script>
